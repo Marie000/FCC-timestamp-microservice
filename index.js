7bcac8b3f566887a933f414c53a2a2eb58590585
Marie Pelletier <marie.pelletier@gmail.com>
2016-07-21 21:24:00 -0400

    trying to fix the favicon issue

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-//app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public"));
 
 app.get('/:date',function(req,res){
   var input = decodeURI(req.params.date);
