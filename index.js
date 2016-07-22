ee7fd889a28f5f618a22aca653db101bffafbc08
Marie Pelletier <marie.pelletier@gmail.com>
2016-07-21 21:55:38 -0400

    trying to deploy to openshift instead

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
 app.use(express.static(__dirname + "/public"));
 
